5c9f7ed4d33b071cb75636f4db02adb779269852
Sulieman Al Rustom <slemanof@gmail.com>
2019-01-25 18:10:16 +0100

    Update app.py
    Commenting

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
 import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
-
+#Declaring Dataset from CVS file uploaded on my Github Python
 data_ica = pd.read_csv('https://raw.githubusercontent.com/Slemanof/Python1/master/SPIndex.csv')
+#Having the firt Column as the Index
 index_col=0
+#Extracting dates from Dateset
 def extract_year(column):
     return column.split('/')[2]
 def extract_month(column):
     return column.split('/')[0]
 data_ica['year'] = data_ica['Date'].apply(extract_year)
 data_ica['month'] = data_ica['Date'].apply(extract_month)
-
+#Calling Dash app HTML
 app = dash.Dash(__name__)
 server = app.server
 server.secret_key = os.environ.get('SECRET_KEY', 'my-secret-key')
-
+#Defining the layout of the output
 app.layout = html.Div(children=[
     html.H1(
         children='S&P 500 Historical Data'
@@ -29,7 +29,14 @@ app.layout = html.Div(children=[
     dcc.Markdown('''
 The Data and the Graphs shows the historical data of S&P 500 Index which is for the American stocks from dates
 1/3/2000 to 12/5/2017.
+The Data are Historical data of S&P Index from 2000 to 2017 downloaded from Yahoo Finance API.
+Dataset could be found here:[Link](https://www.kaggle.com/adityarajuladevi/sp-index-historical-data)
+Project for Programmin Fundamentals done by [*Sulieman Al Rustom*](https://github.com/Slemanof/heroku-project) (Prague College).
+Dataset info:
+Number of Columns: *7*
+Number of Raws: *4511*
     '''
+#Setting up a dropdown list containing the 4 types of charts.
     ),
     html.Div(children=[
         dcc.Dropdown(
@@ -40,7 +47,7 @@ The Data and the Graphs shows the historical data of S&P 500 Index which is for 
                 {'label': 'S&P Volume Heatmap', 'value': 'heatmap'},
 				{'label': 'S&P CandelStick Historical Data', 'value': 'candlestick'}
             ],
-            value='heatmap'
+            value='scatter'
     ),
         dcc.Graph(id= 'graph')
     ])
@@ -50,6 +57,7 @@ The Data and the Graphs shows the historical data of S&P 500 Index which is for 
     Output(component_id='graph', component_property='figure'),
     [Input(component_id='dropinput', component_property='value')]
 )
+#OHLC Chart
 def update_graph(input_value):
     if input_value == 'ohlc':
         trace = go.Ohlc(x=data_ica['Date'],
@@ -69,6 +77,7 @@ def update_graph(input_value):
         )
         data = [trace]
         fig = go.Figure(data=data, layout=layout)
+#Scatter Chart
     elif input_value == 'scatter':
         trace1 = go.Scatter(
             y=data_ica.volume,
@@ -87,15 +96,16 @@ def update_graph(input_value):
         )
         data = [trace1]
         fig = dict(data=data, layout=layout)
+#Heatmap Chart
     elif input_value == 'heatmap':
         trace = go.Heatmap(
                 z = data_ica.volume,
                 x = data_ica.year,
                 y = data_ica.month,
-                
+
                 hoverinfo = 'x+y+z',
-                
-                
+
+
 
         )
         layout = go.Layout(
@@ -106,6 +116,7 @@ def update_graph(input_value):
 
         data=[trace]
         fig= dict(data =data, layout=layout)
+#Candlestick Chart
     else:
 
         trace = go.Candlestick(x=data_ica['Date'],
@@ -128,6 +139,10 @@ def update_graph(input_value):
 
 
     return fig
+ #       _
+ #       .__(.)< (MEOW)
+ #        \___)
+ # ~~~~~~~~~~~~~~~~~~
 
 if __name__ == '__main__':
     app.run_server()
